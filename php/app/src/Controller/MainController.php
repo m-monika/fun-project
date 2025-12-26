cd50438e9f7fbc781b9170ba45f33948326e654d
Monika Młodzik <m852.monika@gmail.com>
2025-12-26 14:36:26 +0100

    run cs-fixer & added to gitignore

--- php/app/src/Controller/MainController.php
+++ php/app/src/Controller/MainController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        $json = ["message" => "Welcome to the homepage!"];
+        $json = ['message' => 'Welcome to the homepage!'];
 
         return $this->json($json);
     }
