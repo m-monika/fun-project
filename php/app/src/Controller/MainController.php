42333a73ef3fa47a9756b28b8578a7beed50d079
Monika Młodzik <m852.monika@gmail.com>
2025-12-26 14:03:11 +0100

    better way for json

--- php/app/src/Controller/MainController.php
+++ php/app/src/Controller/MainController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        $response = new Response('{"message": "Welcome to the homepage!"}');
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
+        $json = ["message" => "Welcome to the homepage!"];
+
+        return $this->json($json);
     }
 }
